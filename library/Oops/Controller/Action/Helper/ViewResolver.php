4f01665b3eada1f95e5c02a3c1643dce2fc96461
Alexandre Segura <mex.zktk@gmail.com>
2011-12-07 21:43:14 +0100

    Removed old code

--- library/Oops/Controller/Action/Helper/ViewResolver.php
+++ library/Oops/Controller/Action/Helper/ViewResolver.php
@@ -10,6 +10,11 @@ class Oops_Controller_Action_Helper_ViewResolver extends Zend_Controller_Action_
         $smartyView	 = new Oops_View_Smarty();
         $smartyView->addScriptPath($defaultView->getScriptPaths());
         
+        // TODO
+        // Move this in another place !
+        $defaultView->addHelperPath('Oops/View/Helper', 'Oops_View_Helper');
+        $smartyView->addHelperPath('Oops/View/Helper', 'Oops_View_Helper');
+        
         $viewsBySuffix = array(
         	'tpl' 	=> $smartyView, 
         	'phtml'	=> $defaultView 
@@ -27,15 +32,31 @@ class Oops_Controller_Action_Helper_ViewResolver extends Zend_Controller_Action_
 	            
 	            $viewRenderer->init();
 	            
+		        $this->initResponseSegment($viewRenderer);
+	            
 	            return;
 	            
 	        }
 	        
         }
         
+    	$this->initResponseSegment($viewRenderer);
+        
         // Restore to default suffix if not found
         $viewRenderer->setViewSuffix('phtml');
 
+    }
+    
+    private function initResponseSegment($viewRenderer) {
+    	
+    	$namespace = 
+        	$this->getActionController()->getInvokeArg('bootstrap')->getOption('appnamespace');
+        	
+    	$request = $this->getActionController()->getRequest();
+		if ($request->getModuleName() == 'front-office') {
+			$viewRenderer->setResponseSegment($namespace . '-' . $request->getActionName());
+		}
+		
     }
     
 }
