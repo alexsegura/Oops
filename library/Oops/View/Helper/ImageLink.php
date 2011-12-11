2f9a88a8108a748430b78ee2b87d922f45c67bc3
Alexandre Segura <mex.zktk@gmail.com>
2011-12-11 15:33:24 +0100

    Fixed strange behavior of LinkCore in ajax requests

--- library/Oops/View/Helper/ImageLink.php
+++ library/Oops/View/Helper/ImageLink.php
@@ -4,12 +4,34 @@ class Oops_View_Helper_ImageLink {
 	
 	public function imageLink($product) {
 		
-		$link 			= new Link();
-		$filterByCover 	= Oops_Model_ImageQuery :: create()->filterByCover(1);
-		$image 			= $product->getImages($filterByCover)->get(0);
+		global $protocol_content;
 		
-		return $link->getImageLink($product->getLinkRewrite(), 
-			$product->getIdProduct() . '-' . $image->getIdImage(), 'medium');
+		$link 			= new Link();
+		$filterByCover 	= Oops_Db_ImageQuery :: create()->filterByCover(1);
+		$images 		= $product->getImages($filterByCover);
+		
+		if ($images->count() >= 1) {
+			
+			$image = $images->get(0);
+			
+			$lnk = $link->getImageLink($product->getLinkRewrite(), 
+				$product->getIdProduct() . '-' . $image->getIdImage(), 'medium');
+			
+			// FIXME
+			// The LinkCore class uses $protocol_content, which is not
+			// always defined when FrontController is not used (case on 
+			// ajax requests). The piece of code below makes sure the 
+			// protocol is always prepended to the link. 
+			$protocol = Tools :: getProtocol();
+			if (null == $protocol_content && false === strpos($lnk, $protocol)) {
+				$lnk = $protocol . $lnk;
+			}
+				
+			return $lnk;
+			
+		}
+		
+		return "";
 		
 	}
 	
