14a2690a83bcd2af2030fe6d49d37f93bb72ae54
Alexandre Segura <mex.zktk@gmail.com>
2011-12-13 21:20:16 +0100

    Added Oops_Application_Tab class

---
+++ library/Oops/Application/Tab.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Oops custom AdminTabCore implementation.
+ * 
+ * Extend this class to be able to run a tab as an MVC application. 
+ * Oops_Application_Tab is able to resolve the module it is associated to 
+ * based on the class name. 
+ * 
+ * Tab class and file must be located directly inside the module folder, 
+ * next to the module class, and respect the following syntax : 
+ * <appnamespace>_Tab_<tabName>
+ * 
+ * This will trigger the corresponding application module in application/controllers : 
+ * <tabName>-tab (Please note that the <tabName> is converted to lower case)
+ * 
+ * @author Alexandre Segura <mex.zktk@gmail.com>
+ */
+abstract class Oops_Application_Tab extends AdminTabCore {
+	
+	protected $module;
+	protected $application;
+	protected $request;
+	
+	public function __construct() {
+		
+		// Inhibit table management
+		$this->table = "";
+		
+		parent :: __construct();
+		
+		// We need to find the module exposed via this tab
+		// Obviously the class namespace is the module name
+		$moduleClass = $this->getNamespace();
+		
+		// TODO Check if class exists !
+		// TODO Check class name syntax, path, etc...
+		$module = Module :: getInstanceByName(strtolower($moduleClass));
+		
+		$this->application 	= $module->getApplication();
+		$this->request		= $module->getRequest();
+		
+	}
+	
+	/**
+	 * Returns the namespace, i.e. the name of the module class. 
+	 * @return string
+	 */
+	public function getNamespace() {
+		return substr(get_class($this), 0, strpos(get_class($this), "_"));
+	}
+	
+	/**
+	 * Returns the last part of the class name in <appnamespace>_Tab_<tabName>
+	 * @return string
+	 */
+	public function getTabName() {
+		return substr(get_class($this), strrpos(get_class($this), "_") + 1);
+	}
+	
+	/**
+	 * Returns the corresponding application module. 
+	 * @return string
+	 */
+	private function getApplicationModuleName() {
+		// TODO Camel case to dash
+		return strtolower($this->getTabName()) . '-tab';
+	}
+	
+	public function display() {
+		try {
+			$this->request->setModuleName($this->getApplicationModuleName());
+			$response = $this->application->getBootstrap()->run();
+			echo (string) $response;
+		} catch (Exception $e) {
+			echo (string) $e;
+		}
+		
+	}
+	
+}
