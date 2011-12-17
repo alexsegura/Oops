5f3681162b3b1b65fcbd9266315459e305cbfe06
Alexandre Segura <mex.zktk@gmail.com>
2011-12-17 18:03:53 +0100

    Added module Bootstrap classes

---
+++ library/Oops/Application/Module/Bootstrap/Preferences.php
@@ -0,0 +1,11 @@
+<?php
+
+class Oops_Application_Module_Bootstrap_Preferences extends Oops_Application_Module_Bootstrap {
+	
+	protected function _initRouter() {
+        $this->bootstrap('FrontController');
+        $front = $this->getResource('FrontController');
+		$front->setRouter('Oops_Controller_Router_Backoffice');
+	}
+	
+}
