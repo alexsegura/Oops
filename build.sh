50dad6e271a433aafb4c63ba1233c7afb93edd13
Alexandre Segura <mex.zktk@gmail.com>
2012-01-11 23:16:03 +0100

    Renamed README file

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ CLASSES_PATH=db/build/classes
 LIBRARY_PATH=library
 
 # rm -f $LIBRARY_PATH/Oops/Model/*
-rm -f $LIBRARY_PATH/Oops/Model/Base/*
+rm -f $LIBRARY_PATH/Oops/Db/Propel/*
 
 # cp $CLASSES_PATH/prestashop/* $LIBRARY_PATH/Oops/Model
-cp $CLASSES_PATH/prestashop/om/* $LIBRARY_PATH/Oops/Model/Base
-cp $CLASSES_PATH/prestashop/map/* $LIBRARY_PATH/Oops/Model
+cp $CLASSES_PATH/prestashop/om/* $LIBRARY_PATH/Oops/Db/Propel
+cp $CLASSES_PATH/prestashop/map/* $LIBRARY_PATH/Oops/Db
 
-find $LIBRARY_PATH/Oops/Model/Base | sed 's/\(.*\)Oops_Model_Base_\(.*\)\(.*\)/mv & \1\2/g' | sh
-# find $LIBRARY_PATH/Oops/Model/ -maxdepth 1 | sed 's/\(.*\)Oops_Model_\(.*\)\(.*\)/mv & \1\2/g' | sh
+find $LIBRARY_PATH/Oops/Db/Propel | sed 's/\(.*\)Oops_Db_Propel_\(.*\)\(.*\)/mv & \1\2/g' | sh
+find $LIBRARY_PATH/Oops/Db/ -name "Oops_Db_*TableMap.php" | sed 's/\(.*\)Oops_Db_\(.*\)\(.*\)/mv & \1\2/g' | sh
 # 
