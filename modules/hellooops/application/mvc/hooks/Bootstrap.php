85c1f5ae75f1903bb99155556dfbfbe356ea5f8b
Alexandre Segura <mex.zktk@gmail.com>
2012-01-11 22:58:52 +0100

    Changed bootstrap

--- modules/hellooops/application/mvc/hooks/Bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HelloOops_Hooks_Bootstrap extends Oops_Application_Module_Bootstrap_Hooks {
-	
-	protected function _initSomething() {
-		
-	}
-	
-}
---
+++ modules/hellooops/application/mvc/preferences/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+class HelloOops_Preferences_Bootstrap extends Oops_Application_Module_Bootstrap_Preferences {
+	
+	protected function _initFoo() {
+		
+	}
+	
+}
